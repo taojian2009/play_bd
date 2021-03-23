064c422796bfcc06461f5c34fd734e8f9ca04ccc
taojian2009 <taojian2009@qq.com>
2021-03-23 15:44:17 +0800

    集合

--- src/Suzu.java
+++ src/Suzu.java
@@ -1,9 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Suzu {
     public static void main(String[] args) {
-        String[] student = {"zoe", "zue"};
-        student[0] = "kevin";
-        System.out.println(student[0]);
-        System.out.println(student.length);
-        System.out.println(student.clone());
+        String[] students = new String[3];
+        for (int i = 0; i < students.length ; i++) {
+            Scanner sc = new Scanner(System.in);
+            String name = sc.next();
+            students[i] = name;
+        }
+
+        System.out.println(Arrays.toString(students));
+
     }
 }
